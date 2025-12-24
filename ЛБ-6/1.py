0538803113101b767237df494d51ba7be5ce4013
gurbanovzaur298-web <gurbanovzaur298@gmail.com>
2025-12-24 14:38:26 +0300

    Delete ЛБ-6 directory

--- ЛБ-6/1.py
+++
@@ -1,3 +0,0 @@
-fio = input("Введите ФИО: ")
-result = ' '.join(word.capitalize() for word in fio.split())
-print(f"Добро пожаловать {result}")
--- ЛБ-6/3.py
+++
@@ -1,8 +0,0 @@
-text = input("Введите текст: ")
-
-step = int(input("Введите шаг: "))
-
-result = text[::step]
-
-print("\nТекст с шагом", step, ":")
-print(result)
